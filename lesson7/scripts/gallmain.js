7a10e1f7ea0e77c4bc1cc43224f0cf23859a918b
samnoorda <noo13002@byui.edu>
2020-02-22 13:20:37 -0700

    finished lesson7

--- lesson7/scripts/gallmain.js
+++ lesson7/scripts/gallmain.js
@@ -61,7 +61,11 @@ WebFont.load({
     }
   });
 
-  let imagesToLoad = document.querySelectorAll('img[data-src]');
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imgOptions = {
+  threshold: 0,
+  rootmargin: "0px 0px 50px 0px"
+};
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
@@ -71,7 +75,7 @@ const loadImages = (image) => {
 
 imagesToLoad.forEach((img) => {
     loadImages(img);
-  });
+});
 
 if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
@@ -81,11 +85,12 @@ if('IntersectionObserver' in window) {
         observer.unobserve(item.target);
       }
     });
-  });
+  }, imgOptions);
   imagesToLoad.forEach((img) => {
     observer.observe(img);
   });
-} else {
+} 
+else {
   imagesToLoad.forEach((img) => {
     loadImages(img);
   });
